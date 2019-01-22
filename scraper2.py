e2a5404ec2129c9ccb0c25068f24faa92d5397cd
mdshepard <mdshepard8@gmail.com>
2019-01-22 10:48:46 -0500

    noticed I never pushed this to github.

--- scraper2.py
+++ scraper2.py
@@ -2,23 +2,40 @@ import re
 import sys
 import requests
 import argparse
+import logging
 from bs4 import BeautifulSoup
 
 
+logger = logging.getLogger(__name__)
+
+
+def setup_logger():
+    """
+    sets up our logger, which will provide input in the terminal as the program
+    runs
+    """
+    fmt = '%(asctime)s:%(levelname)s:%(message)s'
+    logging.basicConfig(format=fmt, level=logging.DEBUG)
+
+
 def find_urls(html_text):
+    """scrapes sites for full urls or relative urls."""
     web_address = []
     soup = BeautifulSoup(html_text.text, 'html.parser')
-    for link in soup.find_all('a'):
-        site = link.get('href')
-        url = re.search(
-            r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
-            str(site))
-        if url:
-            web_address.append(url.group())
+    for w in web_address:
+        for link in soup.find_all('a'):
+            site = link.get('href')
+            url = re.search(
+                r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+                str(site))
+            if url:
+                web_address.append(url.group())
     return web_address
 
 
 def find_phone_nums(html_text):
+    """ scrapes the site for phone numbers. """
+
     nums = []
     text = html_text.text
     phone_nums = re.findall(
@@ -30,6 +47,7 @@ def find_phone_nums(html_text):
 
 
 def find_emails(html_text):
+    """ scrapes site for email addresses """
     email_adrs = []
     text = html_text.text
     emails = re.findall(
@@ -42,6 +60,7 @@ def find_emails(html_text):
 
 
 def find_imgs(html_text):
+    """finds desired img links"""
     pics = []
     soup = BeautifulSoup(
         html_text.text, 'html.parser'
@@ -57,6 +76,7 @@ def find_imgs(html_text):
 
 
 def create_parser():
+    """ creates argument parser"""
     parser = argparse.ArgumentParser()
     parser.add_argument('website',
                         help='website to be scraped!!!'
@@ -67,6 +87,7 @@ def create_parser():
 # Args parsed, website scraped and patterns matched with regex
 def main(args):
     """Parse args, scrape website, scan with regex"""
+    logging.info("scraping {0}.".format(__name__))
     parser = create_parser()
 
     if not args:
@@ -85,7 +106,7 @@ def main(args):
     # Scraped Dem Digitz!
     scraped_phone_nums = find_phone_nums(html_text)
     for phone_num in scraped_phone_nums:
-        print phone_num[0] + "-" + phone_num[1] + "-" + phone_num[2]
+        print(phone_num[0] + "-" + phone_num[1] + "-" + phone_num[2])
 
     # emails scraped!
     scraped_emails = find_emails(html_text)
